05f8246847bb96d284f776cc08e41b48e3e2894c
Felisong <carolinahs100@gmail.com>
2024-11-25 22:46:16 -0700

    got the current time working, I made the mistake of trying to do logger() inside the console log for like 5 minutes lol

--- index.js
+++ index.js
@@ -5,20 +5,37 @@ const path = require("node:path");
 const app = express();
 const port = 5000;
 
+// Middleware current time
+function currentTime() {
+  const now = new Date();
+  const hours = now.getHours();
+  const minutes = now.getMinutes();
+  const days = now.getDate();
+  const months = now.getMonth();
+  const year = now.getFullYear();
+
+  return `${hours.toString().padStart(2, "0")}:${minutes
+    .toString()
+    .padStart(2, "0")}--${days} ${months} ${year}`;
+}
+
+function logger(req, res, next) {
+  console.log(`Time: ${currentTime()}`);
+  console.log(`method: ${req.method}`);
+  console.log(`path: ${req.originalUrl}`);
+  next();
+}
+
+app.use(logger);
+
+// ROUTING CODE
+
 app.get("/", (req, res) => {
   res.send("Meow!");
 });
 
 app.get("/arr", (req, res) => {
   res.send([2, 4, 6, 10, 12, 14, 18, 20, 22]);
-});
-
-// this didnt work quite as intended.
-app.get("/object-test", (req, res) => {
-  res.send([
-    { id: "123?", name: "Kinten", species: "kittyyy :D" },
-    { id: "321", name: "Sethy", species: "my old cat" },
-  ]);
 });
 
 app.listen(port, () => {
