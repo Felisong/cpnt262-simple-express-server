ca0104b796911e0b50e52ee0ed0c1211d752d720
Felisong <carolinahs100@gmail.com>
2024-11-25 22:12:14 -0700

    got it set up.

--- index.js
+++ index.js
@@ -9,6 +9,18 @@ app.get("/", (req, res) => {
   res.send("Meow!");
 });
 
+app.get("/arr", (req, res) => {
+  res.send([2, 4, 6, 10, 12, 14, 18, 20, 22]);
+});
+
+// this didnt work quite as intended.
+app.get("/object-test", (req, res) => {
+  res.send([
+    { id: "123?", name: "Kinten", species: "kittyyy :D" },
+    { id: "321", name: "Sethy", species: "my old cat" },
+  ]);
+});
+
 app.listen(port, () => {
   console.log(`Listening on http://localhost:${port}`);
 });
