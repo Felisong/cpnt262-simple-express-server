f3384a390f56305c49eb46ad05b97ab32f510cf6
Felisong <carolinahs100@gmail.com>
2024-11-25 22:06:07 -0700

    got basic page up.

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
+"use strict";
+
 const express = require("express");
 const path = require("node:path");
 const app = express();
 const port = 5000;
+
+app.get("/", (req, res) => {
+  res.send("Meow!");
+});
+
+app.listen(port, () => {
+  console.log(`Listening on http://localhost:${port}`);
+});
