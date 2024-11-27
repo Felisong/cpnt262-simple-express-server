8aaa72558cf5e8ea4eb02d4c3956ebc7cfcfa293
Felisong <carolinahs100@gmail.com>
2024-11-26 22:31:01 -0700

    i lost 30 minutes of work but now I know how to do it

--- index.js
+++ index.js
@@ -5,41 +5,16 @@ const path = require("node:path");
 const app = express();
 const port = 5000;
 
-// Middleware current time
-function currentTime() {
-  const now = new Date();
-  const hours = now.getHours();
-  const minutes = now.getMinutes();
-  const days = now.getDate();
-  const months = now.getMonth();
-  const year = now.getFullYear();
-
-  return `${hours.toString().padStart(2, "0")}:${minutes
-    .toString()
-    .padStart(2, "0")}--${days} ${months} ${year}`;
-}
-
-function logger(req, res, next) {
-  console.log(`Time: ${currentTime()}`);
-  console.log(`method: ${req.method}`);
-  console.log(`path: ${req.originalUrl}`);
-  next();
-}
-
-app.use(logger);
-
 // test
 const route = path.join(__dirname, "public");
 const pages = path.join(__dirname, "pages");
 
 // SERVER STATIC ASSETS
-app.use(express.static(route));
-app.use(express.static(path));
-
-//test
-app.use(express.Router);
-
 // ROUTING CODE
+app.use(express.static(route));
+app.use(express.static(pages));
+
+// SERVING
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "pages", "index.html"));
 });
@@ -48,16 +23,12 @@ app.get("/about", (req, res) => {
   res.sendFile(path.join(pages, "about.html"));
 });
 
-app.get("/arrApi", (req, res) => {
-  res.send([2, 4, 6, 10, 12, 14, 18, 20, 22]);
-});
-
 app.use((req, res, next) => {
-  res.status(404).send("Page Not Found"); // Or render a custom 404 page
+  res.status(404).sendFile(path.join(pages, "404.html")); // Or render a custom 404 page
 });
 app.use((err, req, res, next) => {
   console.error(`Unhandled Error: ${err}`);
-  res.status(500).send("Internal Server Error");
+  res.status(500).sendFile(path.join(pages, "pages", "404.html"));
 });
 
 // app. listen
